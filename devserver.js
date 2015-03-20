21a2f370060103c860cd6a1fd98678a8311f379e
Babos Csaba <babosc@gmail.com>
2015-03-20 20:22:22 +0100

    Added bump task

--- devserver.js
+++ devserver.js
@@ -6,11 +6,40 @@ var cp = require('child_process');
 var settings = {
 	source: './public/',
 	destination: __dirname + '/www/',
-
 	runport: 9000
 };
 
+var bumpHelp = function() {
+	var text = 'To bump version you need to give the type of the change: ' +
+		'patch, minor, major'; 
+	console.log(text);
+};
+
 switch (process.argv[2]) {
+	case 'bump': 
+		var semver = require('semver');
+		var releaseTypes = ['patch', 'minor', 'major'];
+		var packageJSON = require('./package.json');
+		var version = packageJSON.version;
+		var releaseType = process.argv[3];
+		
+		// If we don't given correct release type then show help and exit
+		if (!releaseTypes.some(function(current) {
+			return releaseType === current;
+		})) {
+			bumpHelp();
+			process.exit(0);
+		}
+
+		var newVersion = semver.inc(version, releaseType);
+		packageJSON.version = newVersion;
+		require('fs').writeFileSync('./package.json', 
+			JSON.stringify(packageJSON, null, 2)
+		);
+
+		cp.execSync('git add package.json && git commit -m "' + newVersion + '"');
+		cp.execSync('git tag v' + newVersion); 
+	break;
 	case 'compile': 
 		console.log('--- cbabos.yourweb.hu ---\n\nTrying to compile the site...');
 		harp.compile(settings.source, settings.destination, function(errors) {
