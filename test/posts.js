d9af09a4813a6dec1f2fb0f3aeb2f899659a0267
Babos Csaba <babos.csaba@yourweb.hu>
2016-02-15 20:04:35 +0100

    Some minimal requirements added as tests

--- test/posts.js
+++ test/posts.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
 import assert from 'assert';
 import postHandler from '../app/postHandler.js';
+
+const testPost = {
+	title: 'Test Post'
+};
+
+const slugTest = {
+	title: '140.5% success; This is awesome!!!!'
+}
 
 let samplePosts;
 let loadSamplePosts = () => {
@@ -7,20 +16,55 @@ let loadSamplePosts = () => {
 }
 
 describe('Querying posts', () => {
-	it('should get all posts back', () => {
-		loadSamplePosts();
+  it('should get all posts back', () => {
+    loadSamplePosts();
 
-		const posts = postHandler(samplePosts).getPosts()
-		assert.equal(typeof posts, 'object');
-		assert.equal(posts.hasOwnProperty('length'), true);
-		assert.strictEqual(posts.length, 3);
-	});
+    const posts = postHandler(samplePosts).getPosts()
+    assert.equal(typeof posts, 'object');
+    assert.equal(posts.hasOwnProperty('length'), true);
+    assert.strictEqual(posts.length, 3);
+  });
 
-	it('should get `Sample Post` back', () => {
-		loadSamplePosts();
+  it('should get `Sample Post` back', () => {
+    loadSamplePosts();
 
-		const post = postHandler(samplePosts).getPost('sample-post');
-		assert.strictEqual(typeof post, 'object');
-		assert.equal(post.title, 'Sample Post'); 
-	});
+    const post = postHandler(samplePosts).getPost('sample-post');
+    assert.strictEqual(typeof post, 'object');
+    assert.equal(post.title, 'Sample Post'); 
+  });
+
+  it('should add `Test Post` and give back the resulted posts', () => {
+    loadSamplePosts();
+
+    const resultedPosts = postHandler(samplePosts).add(testPost);
+    assert.equal(resultedPosts.length, samplePosts.length + 1);
+  });
+
+  it('should create the correct slug for the slugTest post', () => {
+    loadSamplePosts();
+    const handler = postHandler(samplePosts);
+
+    handler.add(slugTest);
+
+    const post = handler.getPost('success');
+    assert.equal(post.slug, '140.5-percent-success');	
+  });
+
+  it('should create and delete the .md file for the post', () => {
+    loadSamplePosts();
+    const handler = postHandler(samplePosts);
+    let error = false;
+
+    handler.add(testPost);
+
+    try {
+      fs.accessSync('../public/posts/test-post.md');
+      handler.delete(testPost.title);
+    } catch (e) {
+      error = e;		
+    }
+
+    assert.equals(error, false);
+    assert.equals(handler.getPosts().length, samplePosts.length);
+  });
 })
