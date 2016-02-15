b02ed31b6a022f0a561c25badc142374b01d01ba
Babos Csaba <babos.csaba@yourweb.hu>
2016-02-15 22:38:17 +0100

    Finishing posthandler

--- app/postHandler.js
+++ app/postHandler.js
@@ -1,17 +1,54 @@
 'use strict';
 
 import moment from 'moment';
+import slug from 'slug';
+import fs from 'fs';
+
+const postTemplate = '%title%\n===\n\nContent is under upload...\n';
 
 let posts = require('../public/posts.json'); 
 let getPost, getPosts, findPost, addPost, delPost, 
-		isMatching, isInStr, deepCopy;
+		isMatching, isInStr, deepCopy, getSlug, createPostFile;
+
+delPost = (identifier) => {
+	const post = getPost(identifier);
+	
+	try {
+		fs.unlink('./public/posts/' + post.slug + '.md');
+		posts = posts.filter(current => current.slug !== post.slug);
+	} catch (e) {
+		console.log("Error occured deleting post. \n", e);	
+	}
+
+	return posts; 
+}
+
+getSlug = (src) => {
+	let replacement;
+	const mode = 'rfc3986';
+
+	slug.defaults.mode = mode;
+	replacement = slug.defaults.modes[mode].replacement;
+	slug.charmap['%'] = slug.defaults.modes[mode].replacement + 'percent';
+	
+	return slug(src).toLowerCase();
+};
+
+createPostFile = (post) => {
+	fs.writeFileSync('./public/posts/' + post.slug + '.md', 
+		postTemplate.replace(/%title%/, post.title)
+	);	
+};
 
 addPost = (postData) => {
-	posts.push(Object.assign(postData, {
-		slug: 'test-post',
+	Object.assign(postData, {
+		slug: getSlug(postData.title),
 		date: moment().format('Y-M-D hh:mm:ss'),
 		published: false 
-	}));
+	});
+
+	createPostFile(postData);
+	posts.push(postData);
 
 	return posts;
 };
@@ -68,6 +105,6 @@ module.exports = (givenPosts) => {
 		"getPost": getPost,
 		"getPosts": getPosts,
 		"add": addPost,
-		"delPost": delPost
+		"delete": delPost
 	};
 };
